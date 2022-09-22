37a30386f3537d0b0b0f051c91b9bd5c2cc6696e
Cych <punyikrisztina@gmail.com>
2022-09-22 17:28:18 +0100

    SigninScreen

---
+++ frontend/src/screens/SigninScreen.js
@@ -0,0 +1,36 @@
+import { Link, useLocation } from 'react-router-dom';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import { Helmet } from 'react-helmet-async';
+
+export default function SigninScreen() {
+  const { search } = useLocation();
+  const redirectInUrl = new URLSearchParams(search).get('redirect');
+  const redirect = redirectInUrl ? redirectInUrl : '/';
+  return (
+    <Container className="small-container">
+      <Helmet>
+        <title>Sign in</title>
+      </Helmet>
+      <h1 className="my-3">Sign in</h1>
+      <Form>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email</Form.Label>
+          <Form.Control type="email" required></Form.Control>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" required></Form.Control>
+        </Form.Group>
+        <div className="mb-3">
+          <Button type="submit">Sign in</Button>
+        </div>
+        <div className="mb-3">
+          New customer?{' '}
+          <Link to={`/signup?redirect=${redirect}`}>Create account</Link>
+        </div>
+      </Form>
+    </Container>
+  );
+}
